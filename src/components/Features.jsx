2ed1b7c76629c4a38da4d2cec5d3bcb59a4cc1d0
reconjayz <jaichawla263@gmail.com>
2025-07-09 17:22:42 +0530

    completed

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -166,7 +166,7 @@ const Features = () => (
         </BentoTilt>
 
         <BentoTilt className="bento-tilt_2">
-          <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+          <div className="flex size-full flex-col justify-between bg-violet-700 p-5">
             <h1 className="bento-title special-font max-w-64 text-black">
               M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
             </h1>
